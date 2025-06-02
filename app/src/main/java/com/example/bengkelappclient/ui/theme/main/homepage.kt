6886fa7f8dc21954c15b3bec5b14ce99f1ea53c8
rivaldy913 <rivaldynovyandp@gmail.com>
2025-06-02 16:10:49 +0700

    [FE] - Membuat Halaman Booking Servis

--- app/src/main/java/com/example/bengkelappclient/ui/theme/main/homepage.kt
+++ app/src/main/java/com/example/bengkelappclient/ui/theme/main/homepage.kt
@@ -2,20 +2,19 @@ package com.example.bengkelappclient.ui.theme.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.activity.viewModels
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope // Pastikan import ini ada
 import com.example.bengkelappclient.R
-import com.example.bengkelappclient.databinding.ActivityMainBinding
-import com.example.bengkelappclient.ui.theme.auth.LoginActivity
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 class homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        val btnReservasi = findViewById<Button>(R.id.btnReservasi)
+        btnReservasi.setOnClickListener {
+            val intent = Intent(this, BookingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
